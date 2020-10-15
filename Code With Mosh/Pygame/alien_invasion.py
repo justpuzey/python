c486a94b3a6d2b27f7433d4c983cc9a57e6ed23c
justpuzey <justpuzey@gmail.com>
2020-10-15 06:51:54 -0400

    Adding ability to move ship to the right using continuous motion

--- Code With Mosh/Pygame/alien_invasion.py
+++ Code With Mosh/Pygame/alien_invasion.py
@@ -1,26 +1,26 @@
-import sys
+
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
-    # Initalize game and create a screen object.
+    # Initalize game, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Set the background color.
-    bg_color = (230, 230, 230)
+    # Make a ship
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
-
-        # Watch fo rkeyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
---
+++ Code With Mosh/Pygame/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """A class to stor all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settins."""
+        # Screen settins
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
