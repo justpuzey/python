588d03ecbfa79b8c312665652883719599d4400d
justpuzey <justpuzey@gmail.com>
2020-10-17 06:59:14 -0400

    this message was written in terminal

--- Code With Mosh/Pygame/settings.py
+++ Code With Mosh/Pygame/settings.py
@@ -1,12 +1,18 @@
 class Settings():
-    """A class to stor all settings for Alien Invasion."""
+    """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settins."""
-        # Screen settins
+        """Initialize the game's settings."""
+        # Screen settings
         self.screen_width = 1200
-        self.screen_height = 700
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
